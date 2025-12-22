7ced5efb9b1c889844430c554bdff0e8ab834cf8
DilmikaSahan <153601909+DilmikaSahan@users.noreply.github.com>
2025-12-22 15:21:10 +0530

    initial project commit

---
+++ src/auth/auth.js
@@ -0,0 +1,11 @@
+export const saveToken = (accessToken, refreshToken) => {
+  localStorage.setItem('accessToken', accessToken);
+  localStorage.setItem('refreshToken', refreshToken);
+}
+export const getAccessToken = () => {
+  return localStorage.getItem('accessToken');
+}
+export const logout = () => {
+  localStorage.clear();
+}
+
