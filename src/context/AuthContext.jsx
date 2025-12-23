3af10433526c4fe13de0dee8925601df0c2a0ed9
DilmikaSahan <153601909+DilmikaSahan@users.noreply.github.com>
2025-12-23 08:17:05 +0530

    implement authcontext for global authentication

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -3,3 +3,24 @@ import { getAccessToken,logout } from "../auth/auth";
 import { loginAdmin } from "../services/authService";
 
 const AuthContext = createContext();
+export const AuthProvider = ({children}) => {
+    const [isAuthenticated,setIsAuthenticated] = useState(false);
+    useEffect(() => {
+        const token = getAccessToken();
+        setIsAuthenticated(!!token);
+    }, []);
+    const login = async (username,password) => {
+        await loginAdmin(username,password);
+        setIsAuthenticated(true);
+    };
+    const signOut = () => {
+        logout();
+        setIsAuthenticated(false);
+    };
+    return (
+        <AuthContext.Provider value={{isAuthenticated,login,signOut}}>
+            {children}
+        </AuthContext.Provider>
+    );
+};
+export const useAuth = () => useContext(AuthContext);
